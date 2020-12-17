458f3285174d5dcc5008fbfcee06ba2c78fea3a4
dangnhap0935 <70467981+dangnhap0935@users.noreply.github.com>
2020-12-18 00:55:25 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$DBHOST = "sql305.epizy.com"; // Set your database host here
+$DBUSER = "epiz_27487888"; // Set your database user here
+$DBNAME = "epiz_27487888_tintuc"; // Set your database name here
+$DBPW =  "MzXI62FmnKJXKL5"; // Set your database password here
+$BOT_ID = "***";
+$TOKEN = "***";
+$BLOCK_NAME = "uet_chat";
+$JSON = "uet_text";
+$BLOCK_IMAGE = "uet_image";
+$BLOCK_VOID = "uet_void";
+$BLOCK_VIDEO = "uet_video";
+$BLOCK_FILE = "uet_file";
+
+?>
