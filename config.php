e13bb5df5cfb5ac433c4fc9c40d8f5db577f6aae
dangnhap0935 <70467981+dangnhap0935@users.noreply.github.com>
2020-12-18 00:59:15 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,22 @@
+
 <?php
+$DBHOST = getenv('DBHOST'); // Set your database host here
+$DBUSER = getenv('DBUSER'); // Set your database user here
+$DBNAME = getenv('DBNAME'); // Set your database name here
+$DBPW =  getenv('DBPASS'); // Set your database password here
+
+/*
+
+$BLOCK_NAME = "uet_chat";
+$JSON = "uet_text";
+$BLOCK_IMAGE = "uet_image";
+$BLOCK_VOID = "uet_void";
+$BLOCK_VIDEO = "uet_video";
+$BLOCK_FILE = "uet_file";
+$DATHAMGIA = "dathamgia";
+
+
+
 $DBHOST = "sql305.epizy.com"; // Set your database host here
 $DBUSER = "epiz_27487888"; // Set your database user here
 $DBNAME = "epiz_27487888_tintuc"; // Set your database name here
@@ -11,5 +29,5 @@ $BLOCK_IMAGE = "uet_image";
 $BLOCK_VOID = "uet_void";
 $BLOCK_VIDEO = "uet_video";
 $BLOCK_FILE = "uet_file";
-
+*/
 ?>
